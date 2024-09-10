3961fc743d7334a3d6f36f5dfd7b3098b425880b
priyankara3cs <priyankara@3cs.solutions>
2024-09-10 12:29:54 +0530

    update swope sound

--- src/components/sandbox.js
+++ src/components/sandbox.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import useSound from "use-sound";
 import "./sandbox.css";
 
 const Page = ({ offset, gradient, onClick }) => (
@@ -20,15 +21,24 @@ const Page = ({ offset, gradient, onClick }) => (
 
 const App = () => {
   const parallax = useRef(null);
+  const [playSound] = useSound("/sounds/swipe-236674.mp3");
 
   const scroll = (to) => {
     if (parallax.current) {
       parallax.current.scrollTo(to);
+      playSound(); // Play sound on scroll
     }
   };
 
   return (
-    <div style={{ background: "#dfdfdf", height: "100vh", overflow: "hidden" }}>
+    <div
+      style={{
+        background: "#dfdfdf",
+        height: "100vh",
+        overflow: "hidden",
+        cursor: "pointer",
+      }}
+    >
       <Parallax ref={parallax} pages={3} horizontal>
         <Page offset={0} gradient="pink" onClick={() => scroll(1)} />
         <Page offset={1} gradient="teal" onClick={() => scroll(2)} />
