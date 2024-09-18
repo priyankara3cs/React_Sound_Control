2ac48979559bf6e14e9e7415a4108d2413ffa35a
priyankara3cs <priyankara@3cs.solutions>
2024-09-18 17:23:39 +0530

    update

--- src/components/ListPages/list-pages.js
+++ src/components/ListPages/list-pages.js
@@ -4,9 +4,6 @@ import "./list-pages.css";
 
 import Page1 from "../test.js";
 import Page2 from "../load-animation.js";
-import Page3 from "./page3.jsx";
-import Page4 from "./page4.jsx";
-import Page5 from "./page5.jsx";
 
 export default function Chart() {
   const [selectedTab, setSelectedTab] = useState("page1");
@@ -27,12 +24,6 @@ export default function Chart() {
         return <Page1 />;
       case "page2":
         return <Page2 />;
-      case "page3":
-        return <Page3 />;
-      case "page4":
-        return <Page4 />;
-      case "page5":
-        return <Page5 />;
       default:
         return null;
     }
@@ -66,35 +57,6 @@ export default function Chart() {
                 Page 2
               </div>
               <br />
-              {/* <div
-                className={`conserve-chart-01-text-box-3-opacity ${
-                  selectedTab === "page3" ? "selected-text" : ""
-                }`}
-                onClick={() => handleTabClick("page3")}
-                onKeyDown={(e) => handleKeyDown(e, "page3")}
-              >
-                Page 3
-              </div>
-              <br />
-              <div
-                className={`conserve-chart-01-text-box-4-opacity ${
-                  selectedTab === "page4" ? "selected-text" : ""
-                }`}
-                onClick={() => handleTabClick("page4")}
-                onKeyDown={(e) => handleKeyDown(e, "page4")}
-              >
-                Page 4
-              </div>
-              <br />
-              <div
-                className={`conserve-chart-01-text-box-5-opacity ${
-                  selectedTab === "page5" ? "selected-text" : ""
-                }`}
-                onClick={() => handleTabClick("page5")}
-                onKeyDown={(e) => handleKeyDown(e, "page5")}
-              >
-                Page 5
-              </div> */}
             </div>
           </>
         </div>
